6588a795f2105a3e409a8b7f588236ccbbbe4e88
Shauryaditya <shauryadityab@gmail.com>
2023-11-14 18:43:24 +0530

    Authentication

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv= require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes")
+const { notFound, errorHandler} = require("./middleware/errorMiddleware")
 
 
 dotenv.config();
@@ -12,14 +13,14 @@ const app = express();
 app.use(express.json()) // to accept JSON data
 
 
-app.get('/', (req,res) => {
+app.get("/", (req,res) => {
     res.send("API is running")
 })
 
-app.use('/api/user',userRoutes)
-app.get("/api/chat", (req,res) => {
-    res.send(chats);
-})
+app.use("/api/user",userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.get("/api/chat/:id", (req,res) => {
     // console.log(req.params.id)
